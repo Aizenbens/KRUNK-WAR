ff4b97c67df3236bb07e347e61c2c2659dc2bdba
Aizenbens <sheninozen@gmail.com>
2026-07-24 14:34:01 -0700

    Update engine.js

--- public/js/engine.js
+++ public/js/engine.js
@@ -1,5 +1,4 @@
-import * as THREE from "three";
-
+import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
 import Player from "./player.js";
 import Weapon from "./weapon.js";
 import World from "./world.js";
