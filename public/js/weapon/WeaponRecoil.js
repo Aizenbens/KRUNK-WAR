650c4a412ded649ca85a139706cbc7eb071a37d6
Aizenbens <sheninozen@gmail.com>
2026-07-24 15:14:43 -0700

    Update WeaponRecoil.js

--- public/js/weapon/WeaponRecoil.js
+++ public/js/weapon/WeaponRecoil.js
@@ -1,1 +1,55 @@
 
+import * as THREE from "three";
+
+export default class WeaponRecoil {
+
+    constructor(weapon, config) {
+
+        this.weapon = weapon;
+        this.config = config;
+
+        this.current = 0;
+
+        this.side = 0;
+
+    }
+
+    shoot() {
+
+        this.current += this.config.recoil.up;
+
+        this.side +=
+            (Math.random() - 0.5) *
+            this.config.recoil.side;
+
+    }
+
+    update(delta) {
+
+        this.current = THREE.MathUtils.lerp(
+
+            this.current,
+
+            0,
+
+            delta * this.config.recoil.recovery
+
+        );
+
+        this.side = THREE.MathUtils.lerp(
+
+            this.side,
+
+            0,
+
+            delta * this.config.recoil.recovery
+
+        );
+
+        this.weapon.rotation.x = -this.current;
+
+        this.weapon.rotation.z = this.side;
+
+    }
+
+}
