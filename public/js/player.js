89071c060ebdcb4d7d0132e90e570be29546e547
Aizenbens <sheninozen@gmail.com>
2026-07-24 13:53:59 -0700

    Update player.js

--- public/js/player.js
+++ public/js/player.js
@@ -1,1 +1,190 @@
+import * as THREE from "three";
 
+export default class Player {
+
+    constructor(camera) {
+
+        this.camera = camera;
+
+        this.position = new THREE.Vector3(
+            0,
+            2,
+            5
+        );
+
+        this.velocity = new THREE.Vector3();
+
+        this.direction = new THREE.Vector3();
+
+        this.speed = 8;
+
+        this.sprintSpeed = 14;
+
+        this.currentSpeed = this.speed;
+
+        this.jumpForce = 8;
+
+        this.gravity = 20;
+
+        this.canJump = true;
+
+        this.keys = {};
+
+        this.pitch = 0;
+
+        this.yaw = 0;
+
+        this.mouseSensitivity = 0.002;
+
+        this.setupKeyboard();
+
+        this.setupMouse();
+
+    }
+
+    setupKeyboard() {
+
+        window.addEventListener("keydown",(e)=>{
+
+            this.keys[e.code]=true;
+
+        });
+
+        window.addEventListener("keyup",(e)=>{
+
+            this.keys[e.code]=false;
+
+        });
+
+    }
+
+    setupMouse(){
+
+        document.addEventListener("click",()=>{
+
+            document.body.requestPointerLock();
+
+        });
+
+        document.addEventListener("mousemove",(e)=>{
+
+            if(document.pointerLockElement!==document.body)
+                return;
+
+            this.yaw-=e.movementX*this.mouseSensitivity;
+
+            this.pitch-=e.movementY*this.mouseSensitivity;
+
+            const limit=Math.PI/2-0.05;
+
+            this.pitch=Math.max(
+                -limit,
+                Math.min(limit,this.pitch)
+            );
+
+        });
+
+    }
+      update(delta) {
+
+        this.direction.set(0,0,0);
+
+        if(this.keys["KeyW"])
+            this.direction.z -= 1;
+
+        if(this.keys["KeyS"])
+            this.direction.z += 1;
+
+        if(this.keys["KeyA"])
+            this.direction.x -= 1;
+
+        if(this.keys["KeyD"])
+            this.direction.x += 1;
+
+        this.direction.normalize();
+
+        this.currentSpeed =
+            this.keys["ShiftLeft"] ?
+            this.sprintSpeed :
+            this.speed;
+
+        const forward = new THREE.Vector3(
+
+            Math.sin(this.yaw),
+            0,
+            Math.cos(this.yaw)
+
+        );
+
+        const right = new THREE.Vector3(
+
+            forward.z,
+            0,
+            -forward.x
+
+        );
+
+        this.position.addScaledVector(
+
+            forward,
+
+            -this.direction.z *
+            this.currentSpeed *
+            delta
+
+        );
+
+        this.position.addScaledVector(
+
+            right,
+
+            this.direction.x *
+            this.currentSpeed *
+            delta
+
+        );
+
+        if(
+            this.keys["Space"] &&
+            this.canJump
+        ){
+
+            this.velocity.y =
+                this.jumpForce;
+
+            this.canJump = false;
+
+        }
+
+        this.velocity.y -=
+            this.gravity * delta;
+
+        this.position.y +=
+            this.velocity.y * delta;
+
+        if(this.position.y < 2){
+
+            this.position.y = 2;
+
+            this.velocity.y = 0;
+
+            this.canJump = true;
+
+        }
+
+        this.camera.position.copy(
+            this.position
+        );
+
+        this.camera.rotation.order =
+            "YXZ";
+
+        this.camera.rotation.y =
+            this.yaw;
+
+        this.camera.rotation.x =
+            this.pitch;
+
+    }
+
+}
